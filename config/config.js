afc2e2a2ad72050326debdfcd6efe1566c8ab010
Marco Gorak <marcogorak@MarcoGorak.local>
2018-01-07 12:48:09 -0200

    teste

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export default {
+    database:'adotepet',
+    username:'adotepet',
+    password: 'superfacil!',
+    params:{
+        dialect: 'mysql',
+        host: 'adotepet.mysql.dbaas.com.br',
+        define:{
+            underscored: true
+        }
+    },
+    jwtSecret: 'Ad0t3p3t!',
+    jwtSession: { session: false}
+}
